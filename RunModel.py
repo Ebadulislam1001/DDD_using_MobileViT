d50aea870c8624b96bd56f0a74897f578428d81d
Ebadulislam1001 <ebadulislamfarooqi@gmail.com>
2024-05-08 22:23:18 +0530

    added wait state before alarm

--- RunModel.py
+++ RunModel.py
@@ -18,41 +18,43 @@ cv2.namedWindow(" model Test", cv2.WINDOW_NORMAL)
 cv2.resizeWindow(" model Test", window_width, window_height)
 model = RTDETR("best.pt")
 
+sleep_counter = 0
 while cap.isOpened():
-    #read a frame from the video
+    # read a frame from the video
     []
     success, frame = cap.read()
     prev_class = 0.0
     try:
-        if success: 
-            time.sleep(4)
+        if success:
             results = model.predict(frame, conf=0.5)
-            print("printing results")
-            # print(results[0])
             annotated_frame = results[0].plot()
-            #display the frame
+            # display the frame
             cv2.imshow(" model Test", annotated_frame)
             class_label = results[0][0].boxes.cls
             conf_label = results[0][0].boxes.conf
-            print("results[0][0].boxes.cls", class_label)
-            print("results[0][0].boxes.conf", conf_label)
-            if class_label == 1.0 and conf_label > 0.75:
-            # if (prev_class == 1.0 and class_label == 1.0 and conf_label > 0.75):
-                file_path = "loud_alarm.mp3"
-                play_music(file_path)
+            print("class label :", class_label)
+            print("conf_label  :", conf_label)
+            if class_label == 1.0 and conf_label > 0.5:
+                sleep_counter += 1
+                if(sleep_counter >= 10):
+                    file_path = "loud_alarm.mp3"
+                    play_music(file_path)
+                    time.sleep(4)
+            else:
+                sleep_counter = 0
             prev_class = class_label
 
-            #Break the loop if q is pressed
+            # Break the loop if q is pressed
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 print("User pressed 'q'. Exiting loop.")
                 break
-        #Break the loop if the end of the video is reached
+        # Break the loop if the end of the video is reached
         else:
             print("End of video reached. Exiting loop.")
             break
     except:
-            print("Error in processing frame.")
-    #Release the video capture object and close the display window
+        print("Error in processing frame.")
+    # Release the video capture object and close the display window
 
 cap.release()
 cv2.destroyAllWindows()
